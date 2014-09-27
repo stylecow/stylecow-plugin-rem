9b812f60d9282fb6175ef53f7e01e64418bc36c8
oscarotero <oom@oscarotero.com>
2014-09-27 11:18:11 +0200

    updated

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-module.exports = {
-	disable: {
-		firefox: 3.6,
-		explorer: 9.0,
-		safari: 5.0,
-		opera: 11.6,
-		ios: 4.0
-	},
+module.exports = function (stylecow) {
 
-	//Set the default value of a rem (16px)
-	RootBefore: function (root) {
-		root.setData('rem', 16);
-	},
+	stylecow.addTask({
+		disable: {
+			firefox: 3.6,
+			explorer: 9.0,
+			safari: 5.0,
+			opera: 11.6,
+			ios: 4.0
+		},
 
-	//Changes the default value on :root or html selectors
-	RuleBefore: function (rule) {
-		if (rule.hasChild('Selector', [':root', 'html'])) {
-			rule.children('Declaration', 'font-size').forEach(function (declaration) {
-				rule.ancestor('Root').setData('rem', toPixels(declaration.value));
-			});
-		}
-	},
+		//Set the default value of a rem (16px)
+		RootBefore: function (root) {
+			root.setData('rem', 16);
+		},
 
-	//Add the fallback
-	Declaration: function (declaration) {
-		var value = declaration.value;
+		//Changes the default value on :root or html selectors
+		RuleBefore: function (rule) {
+			if (rule.hasChild('Selector', [':root', 'html'])) {
+				rule.children('Declaration', 'font-size').forEach(function (declaration) {
+					rule.ancestor('Root').setData('rem', toPixels(declaration.value));
+				});
+			}
+		},
 
-		if (value.indexOf('rem') === -1) {
-			return false;
-		}
+		//Add the fallback
+		Declaration: function (declaration) {
+			var value = declaration.value;
 
-		declaration.cloneBefore().value = value.replace(/([0-9\.]+)rem/, function (match) {
-			if (match[0] === '.') {
-				match = '0' + match;
+			if (value.indexOf('rem') === -1) {
+				return false;
 			}
 
-			return (declaration.getData('rem') * parseFloat(match, 10)) + 'px';
-		});
-	}
+			declaration.cloneBefore().value = value.replace(/([0-9\.]+)rem/, function (match) {
+				if (match[0] === '.') {
+					match = '0' + match;
+				}
+
+				return (declaration.getData('rem') * parseFloat(match, 10)) + 'px';
+			});
+		}
+	});
 };
 
 function toPixels (value) {
