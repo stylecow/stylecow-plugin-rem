44456be37e941021b822f71b7d11f38c1b1a4b3a
oscarotero <oom@oscarotero.com>
2014-12-22 00:07:22 +0100

    api changes

--- index.js
+++ index.js
@@ -1,44 +1,62 @@
 module.exports = function (stylecow) {
 
-	stylecow.addTask({
-		disable: {
-			firefox: 3.6,
-			explorer: 9.0,
-			safari: 5.0,
-			opera: 11.6,
-			ios: 4.0
-		},
+	stylecow.forBrowsersLowerThan({
+		firefox: 3.6,
+		explorer: 9.0,
+		safari: 5.0,
+		opera: 11.6,
+		ios: 4.0
+	}, function () {
 
 		//Set the default value of a rem (16px)
-		RootBefore: function (root) {
-			root.setData('rem', 16);
-		},
+		stylecow.addTask({
+			filter: {
+				type: 'Root'
+			},
+			executeBefore: true,
+			fn: function (root) {
+				root.setData('rem', 16);
+			}
+		});
+
 
 		//Changes the default value on :root or html selectors
-		RuleBefore: function (rule) {
-			if (rule.firstChild({type: 'Selectors'}).hasChild({type: 'Selector', string: [':root', 'html']})) {
-				rule.firstChild({type: 'Block'}).children({type: 'Declaration', name: 'font-size'}).forEach(function (declaration) {
-					rule.parent({type: 'Root'}).setData('rem', toPixels(declaration.join(', ')));
-				});
+		stylecow.addTask({
+			filter: {
+				type: 'Rule'
+			},
+			executeBefore: true,
+			fn: function (rule) {
+				if (rule.firstChild({type: 'Selectors'}).hasChild({type: 'Selector', string: [':root', 'html']})) {
+					rule.firstChild({type: 'Block'}).children({type: 'Declaration', name: 'font-size'}).forEach(function (declaration) {
+						rule.parent({type: 'Root'}).setData('rem', toPixels(declaration.join(', ')));
+					});
+				}
 			}
-		},
+		});
+
 
 		//Add the fallback
-		Declaration: function (declaration) {
-			var code = declaration.toString();
+		stylecow.addTask({
+			filter: {
+				type: 'Declaration'
+			},
+			fn: function (declaration) {
+				var code = declaration.toString();
 
-			if (code.indexOf('rem') === -1) {
-				return false;
-			}
-
-			declaration.before(stylecow.Declaration.createFromString(code.replace(/([0-9\.]+)rem/, function (match) {
-				if (match[0] === '.') {
-					match = '0' + match;
+				if (code.indexOf('rem') === -1) {
+					return false;
 				}
 
-				return (declaration.getData('rem') * parseFloat(match, 10)) + 'px';
-			})));
-		}
+				declaration.before(stylecow.Declaration.createFromString(code.replace(/([0-9\.]+)rem/, function (match) {
+					if (match[0] === '.') {
+						match = '0' + match;
+					}
+
+					return (declaration.getData('rem') * parseFloat(match, 10)) + 'px';
+				})));
+			}
+		});
 	});
 };
 
